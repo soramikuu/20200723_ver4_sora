ad405d3d535187963f0c4b788faf9313e131c681
soramikuu <soramikuu72@gmail.com>
2020-07-24 03:32:53 +0900

    dangerCounterのクリック判定追加

--- 0723_rendav3_Nori/Assets/Scripts/DangerCounter.cs
+++ 0723_rendav3_Nori/Assets/Scripts/DangerCounter.cs
@@ -1,34 +1,70 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
 
 public class DangerCounter : MonoBehaviour
 {
     // カウント用変数
-    public int dangerCnt = 0;
-    public int dangerLimit = 20;
+    public float dangerCnt = 0f;
+    public float dangerLimit = 20f;
+
+    // クリックした時間保存用の変数
+    double clickTimeAt;
+
+    // ダブルクリックと判定するクリック間隔
+    public double asDoubleClickTime = 0.25;
+
+    //private float clickInterval = 0.75f;
+
+    //public float currentTimeClick;
+    //private float lastTimeClick;
 
     // Start is called before the first frame update
     void Start()
     {
+        // 現在時間で初期化
+        clickTimeAt = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // マウスがクリックされたら
+         
+        //毎フレームごとにカウント減らす
+        dangerCnt -= Time.deltaTime;
+
+        //カウントはマイナスにはしない
+        if (dangerCnt <= 0f)
+        {
+            dangerCnt = 0f;
+        }
+
         if (Input.GetMouseButtonDown(0))
         {
-            // カウントを減らす
-            dangerCnt++;
+            // キーを押した間隔を時間から算出
+            double interval = Time.time - clickTimeAt;
+
+            // 間隔がダブルクリック判定時間以下であれば
+            if (interval < asDoubleClickTime)
+            {
+                dangerCnt++;
+            }
+
+            // クリックした時間を保存
+            clickTimeAt = Time.time;
         }
+
 
         //カウントをTextコンポーネントへ 
         this.GetComponent<Text>().text = "保水力は" + dangerCnt + "%です";
 
-        // もしdangerCntが20以上になったら
+        // もしdangerCntがdangerLimit以上になったら
         if (dangerCnt == dangerLimit)
         {
             // カウントを0に戻す
@@ -36,5 +72,22 @@ public class DangerCounter : MonoBehaviour
             // this.GetComponent<Text>().text = "結果は" + result + "秒でした";
             SceneManager.LoadScene("ResultScene");
         }
+
+        
     }
+
+    //public void OnPointerClick(PointerEventData eventData)
+    //{
+    //    currentTimeClick = eventData.clickTime;
+    //    //Debug.Log(currentTimeClick);
+    //    if (Mathf.Abs(currentTimeClick - lastTimeClick) <= clickInterval)
+    //    {
+    //        // カウントを増やす
+    //        dangerCnt++;
+    //    }
+    //    lastTimeClick = currentTimeClick;
+    //}
+
+
+
 }
